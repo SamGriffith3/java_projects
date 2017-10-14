fda8613471d594313ea8f4a9cd03434d872c91d0
Sam Griffith <garfunkelnator@gmail.com>
2017-10-14 12:56:43 -0400

    Create crypto_trader.java

---
+++ crypto_trader.java
@@ -0,0 +1,26 @@
+
+/* Java is a whole new ballpark for me, so this is just a practice space. Please DO NOT use this on your system, as a banana on your
+processor might be better for your computer than this code is. I refute any responsibility for taking care of your messed up system if you 
+do use any of this code for any purpose */
+
+
+public Class Crypto_Trader{
+
+public status void main(String[] args) {
+
+int bitcoin = 0;
+int litecoin = 0;
+int ethereum = 0;
+int zcash = 0;
+
+if (bitcoin < litecoin){
+    
+
+
+
+
+
+
+}
+}
+
